d8feab80f8aac4a6ef52d962117c56b2e3ed0ce7
Steven Yee <steveyee@cisco.com>
2017-04-18 11:22:31 -0700

    Begin work on wrapper

---
+++ step2/IOTWrapper.py
@@ -0,0 +1,11 @@
+class IOTWrapper():
+
+    def IOTWrapper(self, maxSize, highValue, lowValue, roomID, bearer):
+        self.maxSize = maxSize
+        self.highValue = highValue
+        self.lowValue = lowValue
+        self.roomID = roomID
+        self.bearer = bearer
+        self.arrayOfValues = []
+
+    

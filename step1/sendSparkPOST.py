21f55c7ee48d05447ca9cbc091b7ba392eebc43b
Steven Yee <steveyee@cisco.com>
2017-04-18 10:03:24 -0700

    Add Spark POST method

---
+++ step1/sendSparkPOST.py
@@ -0,0 +1,10 @@
+import urllib2
+import json
+
+def sendSparkPOST(url, data):
+    request = urllib2.Request(url, json.dumps(data),
+        headers={"Accept" : "application/json",
+                 "Content-Type":"application/json"})
+    request.add_header("Authorization", "Bearer "+ bearer)
+    contents = urllib2.urlopen(request).read()
+    return contents

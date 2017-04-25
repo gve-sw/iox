feb30bdab59fb29d5ba65c4525c2ead8b69514c8
Steven Yee <steveyee@cisco.com>
2017-04-25 00:43:49 -0700

    Poll sensor in real time

--- step3/iot-developer-demo-kit-python-master/Pi_senseHat/www/app.py
+++ step3/iot-developer-demo-kit-python-master/Pi_senseHat/www/app.py
@@ -14,6 +14,7 @@ import sys
 import argparse
 import urllib2
 import json
+import time
 
 app = Flask(__name__, static_url_path='', static_folder="static")
 # roomId = "Y2lzY29zcGFyazovL3VzL1JPT00vMjIyYTE3MjAtMjMwMi0xMWU3LWE1ZDMtYmQ4ODU5YjY3ODc4"
@@ -21,7 +22,7 @@ app = Flask(__name__, static_url_path='', static_folder="static")
 # maxSize = 100
 # highValue = 120.0
 # lowValue = 60.0
-myWrapper = None
+global myWrapper
 
 @app.route('/status')
 def status():
@@ -38,39 +39,67 @@ def get_data(type):
         t[type] = '-1'
     return jsonify({'ts': int(time.time() *1000), type:t})
 
-def sendSparkPOST(url, data):
-    """
-    This method is used for:
-        -posting a message to the Spark room to confirm that a command was received and processed
-    """
-    request = urllib2.Request(url, json.dumps(data),
-                            headers={"Accept" : "application/json",
-                                     "Content-Type":"application/json"})
-    request.add_header("Authorization", "Bearer "+bearer)
-    contents = urllib2.urlopen(request).read()
-    return contents
+# def sendSparkPOST(url, data):
+#     """
+#     This method is used for:
+#         -posting a message to the Spark room to confirm that a command was received and processed
+#     """
+#     request = urllib2.Request(url, json.dumps(data),
+#                             headers={"Accept" : "application/json",
+#                                      "Content-Type":"application/json"})
+#     request.add_header("Authorization", "Bearer "+bearer)
+#     contents = urllib2.urlopen(request).read()
+#     return contents
 
 # def sendSparkMessage(roomId, message):
 #     sendSparkPOST("https://api.ciscospark.com/v1/messages", {"roomId": roomId, "text": message})
 
-@app.route('/test')
-def test_messages(message):
-    myWrapper.sendSparkMessage(message)
+# @app.route('/test')
+# def test_messages(message):
+#     myWrapper.sendSparkMessage(message)
+
+# @app.route('/generate')
+# def pull_temperature():
+#     message = sensor.get_data('temperature')
+#     myWrapper.sendSparkMessage(message)
+#     return app.send_static_file('index.html')
 
 @app.route('/generate')
 def pull_temperature():
-    message = sensor.get_data('temperature')
-    myWrapper.sendSparkMessage(message)
+    print "GENERATING"
+    global myWrapper
+    temperature = sensor.get_data('temperature')
+    print temperature
+    myWrapper.addValue(temperature)
+    average = myWrapper.averageValues()
+    if myWrapper.monitorSize():
+        if myWrapper.monitorAverage():
+            if average >= myWrapper.getHigh():
+                myWrapper.sendSparkMessage("HOT DAMN\n")
+                myWrapper.sendSparkMessage("It's steamy in here.")
+            else:
+                myWrapper.sendSparkMessage("BRRRRRRRRR\n")
+                myWrapper.sendSparkMessage("It's freezing in here")
+            myWrapper.sendSparkMessage("There is an extreme average temperature of " + str(average) + " degrees Fahrenheit over the past " + str(myWrapper.getMaxSize() * 3) + " seconds!!!")
+        else:
+            myWrapper.sendSparkMessage("It's all good in the hood")
     return app.send_static_file('index.html')
 
+
 @app.route('/initialize')
-def initializeSensor(maxSize = 100, highValue = 120.0, lowValue = 60.0, roomId = "Y2lzY29zcGFyazovL3VzL1JPT00vMjIyYTE3MjAtMjMwMi0xMWU3LWE1ZDMtYmQ4ODU5YjY3ODc4", bearer = "YzY2ZTMzNWEtOWVkYy00NDlhLThkZmYtZTNiZDQ3NTkyYTdjMDFkNjkzNTEtY2Y3"):
+def initializeSensor(maxSize = 5, highValue = 120.0, lowValue = 60.0, roomId = "Y2lzY29zcGFyazovL3VzL1JPT00vMjIyYTE3MjAtMjMwMi0xMWU3LWE1ZDMtYmQ4ODU5YjY3ODc4", bearer = "YzY2ZTMzNWEtOWVkYy00NDlhLThkZmYtZTNiZDQ3NTkyYTdjMDFkNjkzNTEtY2Y3"):
     # message = sensor.get_data('temperature')
+    global myWrapper
+    print "INITIALIZED"
     myWrapper = IOTWrapper(maxSize, highValue, lowValue, roomId, bearer)
-    myWrapper.sendSparkMessage("I am initialized with an interval of " + str(maxSize) + " seconds\n"
+    myWrapper.sendSparkMessage("I am initialized with an interval of " + str(maxSize * 3) + " seconds\n"
         "a high temperature of " + str(highValue) + " degrees Fahrenheit\n"
         "and a low temperature of " + str(lowValue) + " degrees Fahrenheit\n"
         )
+    while True:
+        print "Loop"
+        pull_temperature()
+        time.sleep(3)
 
     return app.send_static_file('index.html')
 
@@ -91,10 +120,8 @@ if __name__ == '__main__':
     parser.add_argument('-v', '--version', action='version', version='1.0')
     args = parser.parse_args()
     try:
+        # initializeSensor()
         app.run(host='0.0.0.0', port=args.port, debug=True)
-        # while True:
-
-
 
     except Exception as e:
         print 'Flask Launch Error! Check port number'

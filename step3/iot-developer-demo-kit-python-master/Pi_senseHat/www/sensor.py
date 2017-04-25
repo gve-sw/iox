43ac4fa1a91cc34cbc1f30e39b595ffdec4a9d1b
Steven Yee <steveyee@cisco.com>
2017-04-25 00:02:25 -0700

    Simulate sensor data

--- step3/iot-developer-demo-kit-python-master/Pi_senseHat/www/sensor.py
+++ step3/iot-developer-demo-kit-python-master/Pi_senseHat/www/sensor.py
@@ -5,9 +5,36 @@
 # 
 # DESCRIPTION
 #     Raspberry Sensorhat sensor operations.
-from sense_hat import SenseHat
+# from sense_hat import SenseHat
+from random import randint
+
+class SenseHat(object):
+    def get_temperature(self):
+
+        temperatureData = [{"temperature" : 35.35, "ts" : 1476833997042},
+            {"temperature" : 45.45,
+            "ts" : 1476833997042},{"temperature" : 55.55,
+            "ts" : 1476833997042},{"temperature" : 65.65,
+            "ts" : 1476833997042},{"temperature" : 75.75,
+            "ts" : 1476833997042},{"temperature" : 85.85,
+            "ts" : 1476833997042},{"temperature" : 95.95,
+            "ts" : 1476833997042},{"temperature" : 105.35,
+            "ts" : 1476833997042},{"temperature" : 115.35,
+            "ts" : 1476833997042},{"temperature" : 125.35,
+            "ts" : 1476833997042},{"temperature" : 135.35,
+            "ts" : 1476833997042},{"temperature" : 145.35,
+            "ts" : 1476833997042}
+            ]
+        temperature = temperatureData[randint(0, len(temperatureData))]["temperature"]
+        print(temperature)
+        return temperature
+
+
+
+
+
 sense = SenseHat()
-sense.show_message("Sensehat",text_colour=[0,0,200])
+# sense.show_message("Sensehat",text_colour=[0,0,200])
 
 def get_sim_data(type):
     return {
@@ -32,24 +59,25 @@ def get_data_rounded(raw_data):
 # Get the sensehat data, return None if the item requested is not available    
 def get_data(type):
     return {
-     'temperature':         get_data_rounded(sense.get_temperature()),     
-     'humidity':            get_data_rounded(sense.get_humidity()),       
-     'barometricpressure':  get_data_rounded(sense.get_pressure()),     
-     'magnetometer':        get_data_rounded(sense.get_compass_raw()), 
-     'accelerometer':       get_data_rounded(sense.get_accelerometer()), 
-     'gyroscope':           get_data_rounded(sense.get_gyroscope())
+     'temperature':         get_data_rounded(sense.get_temperature())
+     # ,     
+     # 'humidity':            get_data_rounded(sense.get_humidity()),       
+     # 'barometricpressure':  get_data_rounded(sense.get_pressure()),     
+     # 'magnetometer':        get_data_rounded(sense.get_compass_raw()), 
+     # 'accelerometer':       get_data_rounded(sense.get_accelerometer()), 
+     # 'gyroscope':           get_data_rounded(sense.get_gyroscope())
       }.get(type, 'None')
 
-def display_data(raw_message):
-    if len(raw_message) <= 8:
-        sense.show_message(raw_message,text_colour=[0,0,200])
-    else:           
-        sense.show_message("StrErr!",text_colour=[200,0,0])
+# def display_data(raw_message):
+#     if len(raw_message) <= 8:
+#         sense.show_message(raw_message,text_colour=[0,0,200])
+#     else:           
+#         sense.show_message("StrErr!",text_colour=[200,0,0])
 
-def set_data(type, value = 0):
-    return {
-     'flip_h':              sense.flip_h(),       
-     'flip_v':              sense.flip_v(),
-     'message':             display_data(value),
-     'clear':               sense.clear()
-      }.get(type, 'None')
+# def set_data(type, value = 0):
+#     return {
+#      'flip_h':              sense.flip_h(),       
+#      'flip_v':              sense.flip_v(),
+#      'message':             display_data(value),
+#      'clear':               sense.clear()
+#       }.get(type, 'None')
